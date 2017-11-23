f9b558e1bbe2a7068290b67c8552a682c921ff24
Jun <cygoh@umich.edu>
2017-11-23 13:13:23 -0800

    deleteProject and update state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Projects from './components/Projects';
 import AddProject from './components/AddProject';
+import uuid from 'uuid';
 import './App.css';
 
 class App extends Component {
@@ -14,14 +15,17 @@ class App extends Component {
   componentWillMount() {
     this.setState({projects: [
       {
+        id: uuid.v4(),
         title: 'Business Website',
         category: 'Web Design'
       },
       {
+        id: uuid.v4(),
         title: 'Social App',
         category: 'Mobile Development'
       },
       {
+        id: uuid.v4(),
         title: 'Language App',
         category: 'Mobile Development'
       }
@@ -35,13 +39,20 @@ class App extends Component {
     this.setState({projects: projects});
   }
 
+  handleDeleteProject(id) {
+    let projects = this.state.projects;
+    let index = projects.findIndex(x => x.id === id);
+    projects.splice(index, 1);
+    this.setState({projects: projects});
+  }
+
   render() {
     return (
       <div className="App">
         My App
 
         <AddProject addProject={this.handleAddProject.bind(this)}/>
-        <Projects projects={this.state.projects}/>
+        <Projects projects={this.state.projects} onDelete={this.handleDeleteProject.bind(this)}/>
       </div>
     );
   }
